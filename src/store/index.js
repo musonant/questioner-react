5427a081426a62fc95325598a6f040cf0ef88892
Emmanuel Osuh <mrmusonant@gmail.com>
2019-06-03 04:27:55 +0100

    [Chore #166398540] setup tests

--- src/store/index.js
+++ src/store/index.js
@@ -9,9 +9,10 @@ const store = createStore(
   {},
   compose(
     applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+    (window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()) ||
+      compose
+  )
 );
 
 export default store;
